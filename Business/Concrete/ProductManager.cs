6325a7c998cba72e69260183504384ee1fb294c1
rumeysacelik <rmysclk61@icloud.com>
2021-01-31 00:32:35 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public static bool ProductName { get; set; }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            return _productDal.GetAll();
+            
+        }
+    }
+}
---
+++ Entities/Abstact/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstact
+{
+    //IEntitiy implemente eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
