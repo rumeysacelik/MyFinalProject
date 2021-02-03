a2503e97ed62dadbb89c82aef42ccb1514e0ac7b
rumeysacelik <rmysclk61@icloud.com>
2021-02-04 00:28:15 +0300

    son

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach ( var product in productManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                 Console.WriteLine(product.ProductName);
             }
             
             
